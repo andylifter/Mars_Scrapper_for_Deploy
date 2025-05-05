30be3638001ed0fce0eb0affa36746da30bf34b8
andylifter <kenkokan@mail.ru>
2025-05-05 05:15:03 +0300

    fix_2

--- get_images.py
+++ get_images.py
@@ -6,31 +6,31 @@ import time
 import os
 
 # *************************************************************** get images ***********************************************************#
-count_Curiosity = 0 
-jsondata = json.loads(requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=' + API_KEY).text)
-for i in jsondata['latest_photos']:
-   count_Curiosity += 1
-   link = (i['img_src'])
-   os.makedirs('static/Curiosity', exist_ok=True)
-   dt = str(datetime.datetime.now()).replace(' ', '_').replace('-', '_').replace(':', '_')
-   img_data = requests.get(link).content
-   with open(os.path.join("static/Curiosity", f"{dt}.jpg"), 'ab') as f:
-      f.write(img_data)
+# count_Curiosity = 0 
+# jsondata = json.loads(requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=' + API_KEY).text)
+# for i in jsondata['latest_photos']:
+#    count_Curiosity += 1
+#    link = (i['img_src'])
+#    os.makedirs('static/Curiosity', exist_ok=True)
+#    dt = str(datetime.datetime.now()).replace(' ', '_').replace('-', '_').replace(':', '_')
+#    img_data = requests.get(link).content
+#    with open(os.path.join("static/Curiosity", f"{dt}.jpg"), 'ab') as f:
+#       f.write(img_data)
 
-time.sleep(3)
+# time.sleep(3)
 
-count_Perseverance = 0
-jsondata = json.loads(requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/latest_photos?api_key=' + API_KEY).text)
-for i in jsondata['latest_photos']:
-   count_Perseverance += 1
-   link = (i['img_src'])
-   os.makedirs('static/Perseverance', exist_ok=True)
-   dt = str(datetime.datetime.now()).replace(' ', '_').replace('-', '_').replace(':', '_')
-   img_data = requests.get(link).content
-   with open(os.path.join("static/Perseverance", f"{dt}.jpg"), 'ab') as f:
-      f.write(img_data)
+# count_Perseverance = 0
+# jsondata = json.loads(requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/latest_photos?api_key=' + API_KEY).text)
+# for i in jsondata['latest_photos']:
+#    count_Perseverance += 1
+#    link = (i['img_src'])
+#    os.makedirs('static/Perseverance', exist_ok=True)
+#    dt = str(datetime.datetime.now()).replace(' ', '_').replace('-', '_').replace(':', '_')
+#    img_data = requests.get(link).content
+#    with open(os.path.join("static/Perseverance", f"{dt}.jpg"), 'ab') as f:
+#       f.write(img_data)
 
-time.sleep(3)
+# time.sleep(3)
 
 # **************************************************** clear directory from old images ***********************************************#
 # for file in os.listdir('static/Curiosity'):
